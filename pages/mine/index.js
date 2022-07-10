2c9e9b2590922e93e5798f9130b31e27e03a7d95
wangshubin <123@123.com>
2022-07-10 23:36:12 +0800

    界面开发

--- pages/mine/index.js
+++ pages/mine/index.js
@@ -29,38 +29,7 @@ Page({
 
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
+  handleEditInfo() {
+    wx.navigateTo({ url: '/pages/edit-info/index' })
   }
 })
---
+++ pages/syllable/index.js
@@ -0,0 +1,49 @@
+// pages/syllable/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    winHeight: '100%',
+    toView: 'qingBox',
+    activeMenu: 'qingBox'
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+    const that = this;
+    wx.getSystemInfo({
+      success: function(res) {
+        that.setData({
+          winHeight: res.windowHeight - (res.windowWidth * 90 / 750) + 'px'
+        })
+      },
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  toViewClick: function(e){
+    console.log(e)
+    const that = this
+    that.setData({
+      toView: e.currentTarget.dataset.hash,
+      activeMenu: e.currentTarget.dataset.hash
+    })
+  }
+})

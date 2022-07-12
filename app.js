af4e3ee2669eb69657694260fc0ca80c7dce9cff
wangshubin <123@123.com>
2022-07-12 23:35:20 +0800

    登录登出

--- app.js
+++ app.js
@@ -1,2 +1,34 @@
 // app.js
-App({})
+App({
+  globalData: {
+    userInfo: null,   // 微信用户信息
+  },
+
+  onLaunch: function (option) {
+    this.ready()
+  },
+
+  ready: function() {
+    wx.getSetting({
+      success: res => {
+        // 已经授权
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          // wx.getUserInfo({
+          //   success: res => {
+          //     console.log('打印这里授权====>', res)
+          //     // 可以将 res 发送给后台解码出 unionId
+          //     this.globalData.userInfo = res.userInfo
+
+          //     // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+          //     // 所以此处加入 callback 以防止这种情况
+          //     if (this.userInfoReadyCallback) {
+          //       this.userInfoReadyCallback(res)
+          //     }
+          //   }
+          // })
+        }
+      }
+    })
+  }
+})
--- pages/mine/index.js
+++ pages/mine/index.js
@@ -1,32 +1,72 @@
 // pages/mine/index.js
+const app = getApp()
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    userInfo: null,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
   onShow() {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      this.setData({
+        userInfo: {},
+        hasUserInfo: false
+      })
+    }
+  },
 
+  handleLogin() {
+    const that = this
+    wx.showLoading({ title: '登录中...' })
+    wx.login({
+      success (res) {
+        console.log('获取code====>', res.code)
+        if (res.code) {
+          wx.getUserInfo({
+            success: res => {
+              // console.log('打印这里授权====>', res)
+              // 可以将 res 发送给后台解码出 unionId
+              wx.showToast({
+                title: '登录成功',
+                success: () => {
+                  that.setData({
+                    userInfo: res.userInfo
+                  })
+                }
+              })
+            }
+          })
+        }
+      }
+    })
+  },
+
+  handleLoginOut() {
+    const that = this
+    wx.showLoading({ title: '退出中...' })
+    that.setData({
+      userInfo: null
+    })
+    wx.hideLoading()
   },
 
   handleEditInfo() {

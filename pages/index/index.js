8ecfa83ae4680edf2e710787eaeda4a1a85b0291
wangshubin <shubin1025@aliyun.com>
2022-07-13 21:29:59 +0800

    发起请求demo

--- pages/index/index.js
+++ pages/index/index.js
@@ -9,7 +9,16 @@ Page({
   },
 
   onShow() {
-
+    wx.request({
+      url: 'https://api.chongyaedu.com:8443/api/dataInfo/list', //仅为示例，并非真实的接口地址
+      data: {},
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success (res) {
+        console.log("1111====>", res.data)
+      }
+    })
   },
 
   // 去开始学习
